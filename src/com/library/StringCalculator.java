3cbf6f6ccf1c1f2251209817ae2409c8d795ea7d
Mokshagna Sai Teja Komatipolu <mokshagnasaiteja517@gmail.com>
2021-03-12 20:50:33 +0530

    Add first unit test of passing empty string to method

--- src/com/library/StringCalculator.java
+++ src/com/library/StringCalculator.java
@@ -1,9 +1,18 @@
 package com.library;
 
+
 public class StringCalculator {
 
     public static void main(String[] args) {
 	// write your code here
-    }
+        StringCalculator obj=new StringCalculator();
 
+        if(obj.Add("")==0)
+            System.out.println("Test passed");
+        else
+            System.out.println("Test failed");
+    }
+    public int Add(String numbers){
+        return Integer.MAX_VALUE;
+    }
 }
